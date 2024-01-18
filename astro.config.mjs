b7d3afec6e324da3c036f0f40341cb40941b6175
Matías Gonta <maret2023@gmail.com>
2024-01-18 20:00:10 -0300

    Revert "Revert "Configure Vercel :rocket:""
    This reverts commit aac09ab737be26a87ee891523421ffe70f59d0b2.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,6 @@ export default defineConfig({
     directory: 'assets'
   },
   integrations: [tailwind(), react()],
-  output: 'hybrid'
+  adapter: vercel(),
+  output: 'hybrid',
 });
