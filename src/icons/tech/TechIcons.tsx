3e329affb70901da74fb76434efd3617c657b767
Matías Gonta <maret2023@gmail.com>
2024-01-19 11:52:39 -0300

    Ready for deploy :rocket:

--- src/icons/tech/TechIcons.tsx
+++ src/icons/tech/TechIcons.tsx
@@ -298,11 +298,11 @@ export const DndKitIcon = () => {
                 width="100%"
                 height="100%"
                 viewBox="0 0 24 24"
-                stroke-width="1"
+                strokeWidth="1"
                 stroke="currentColor"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M16 4l4 0l0 4" />
